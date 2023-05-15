937b4d78c003228f7041db23b7a2ef050fbc2596
kwkry <117517261+kwkry@users.noreply.github.com>
2023-05-15 17:56:47 +0800

    Update app_fruit_classification.py

--- app_fruit_classification.py
+++ app_fruit_classification.py
@@ -29,7 +29,7 @@ def main():
     model = load_model()
     class_names = ["Apple", "Banana", "Carambola", 
                    "Guava", "Kiwi", "Mango", 
-                   "Muskmelon", "Orange", "Peach", 
+                   "muskmelon", "Orange", "Peach", 
                    "Pear", "Persimmon", "Pitaya", 
                    "Plum", "Pomegranate", "Tomatoes"]
     
